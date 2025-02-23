9fc4b697e2f1b1c2d3ac294a2391f039c39afea3
Guilhermo Masid <guilhermomasid@gmail.com>
2025-02-23 09:54:59 -0300

    feat: list links

--- lib/blinks_web/live/link_live/index.ex
+++ lib/blinks_web/live/link_live/index.ex
@@ -1,7 +1,13 @@
 defmodule BlinksWeb.LinkLive.Index do
   use BlinksWeb, :live_view
 
+  alias Blinks.Links
+
   def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:links, Links.list_links())
+
     {:ok, socket}
   end
 end
