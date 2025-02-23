6d1e4bd762ddc5285a7ee39258a3aaa7da95d487
Guilhermo Masid <guilhermomasid@gmail.com>
2025-02-23 14:48:00 -0300

    chore: validate form in real time

--- lib/blinks_web/live/link_live/index.ex
+++ lib/blinks_web/live/link_live/index.ex
@@ -18,12 +18,25 @@ defmodule BlinksWeb.LinkLive.Index do
     {:ok, socket}
   end
 
-  def handle_event("submit", %{"link" => link_params}, socket) do
+  def handle_event("validate", %{"link" => params}, socket) do
+    form =
+      %Link{}
+      |> Links.change_link(params)
+      |> to_form(action: :validate)
+
+    socket =
+      socket
+      |> assign(:form, form)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("create", %{"link" => link_params}, socket) do
     case Links.create_link(link_params) do
       {:ok, link} ->
         PubSub.broadcast(Blinks.PubSub, @topic, {:new_link, link})
 
-        changeset = Link.changeset(%Link{url: ""})
+        changeset = Link.changeset(%Link{})
 
         socket =
           socket
@@ -32,7 +45,7 @@ defmodule BlinksWeb.LinkLive.Index do
 
         {:noreply, socket}
 
-      {:error, changeset} ->
+      {:error, %Ecto.Changeset{} = changeset} ->
         socket =
           socket
           |> assign(:form, to_form(changeset))
