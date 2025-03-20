4ca5231ec2bda244f3acec41e714544300c15367
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-20 19:24:34 -0300

    resize

--- lib/blinks_web/components/online_users_count.ex
+++ lib/blinks_web/components/online_users_count.ex
@@ -10,10 +10,10 @@ defmodule BlinksWeb.OnlineUsersCount do
     ~H"""
     <div class="flex items-center justify-between gap-2">
       <p>{@count} people online</p>
-      <span class="relative flex top-0 right-0 size-3">
-        <span class="absolute flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75 [animation-duration:1.5s]">
+      <span class="relative flex top-0 right-0 size-2.5">
+        <span class="absolute flex h-full w-full animate-ping rounded-full bg-green-300 opacity-75 [animation-duration:1.5s]">
         </span>
-        <span class="relative inline-flex size-3 rounded-full bg-green-500"></span>
+        <span class="relative inline-flex size-2.5 rounded-full bg-green-400"></span>
       </span>
     </div>
     """
